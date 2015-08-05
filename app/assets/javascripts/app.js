cb58969702304ce2ba74219b92bf9fbe3e3e08c3
CarsonBills <CarsonMBills@Gmail.com>
2015-08-04 23:03:30 -0400

    angular displays list of dummy items... pretty much done at this point

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,12 @@ angular.module("bumbershoot", [])
 .controller("MainCtrl", [
 "$scope",
 function($scope){
-	$scope.test = "Hello world!";
+	$scope.items = [
+		"item1",
+		"item2",
+		"item3",
+		"item4",
+		"item5"
+	]
 }
 ])
