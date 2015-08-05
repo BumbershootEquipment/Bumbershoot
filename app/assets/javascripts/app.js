edf6a564afdd1ad6dc09206500b1f3b8e09b1b77
CarsonBills <CarsonMBills@Gmail.com>
2015-08-05 08:34:50 -0400

    items exported to factory

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,12 +1,18 @@
 angular.module("bumbershoot", [])
 .controller("MainCtrl", [
 "$scope",
-function($scope){
-	$scope.items = [
-		{name: "tent", category: "sleep", image: "Picture of a tent"},
-		{name: "stove", category: "cook", image: "Picture of a stove"},
-		{name: "lantern", category: "light", image: "Picture of a lantern"},
-		{name: "fishing pole", category: "recreation", image: "Picture of a fishing pole"},
-	]
-}
-])
+"items",
+function($scope, items){
+	$scope.items = items.items;
+}])
+
+.factory("items", [function(){
+	var o = {
+		items: [
+			{name: "tent", category: "sleep", image: "Picture of a tent"},
+			{name: "stove", category: "cook", image: "Picture of a stove"},
+			{name: "lantern", category: "light", image: "Picture of a lantern"},
+			{name: "fishing pole", category: "recreation", image: "Picture of a fishing pole"}
+		]};
+	return o;
+}])
