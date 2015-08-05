ede073df87f7fdc06aaead4b0c6444a4be0535ef
CarsonBills <CarsonMBills@Gmail.com>
2015-08-05 18:57:52 -0400

    writes controller methods

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -1,2 +1,18 @@
 class DescriptionsController < ApplicationController
+
+  def create
+    item = Item.find(params[:item_id])
+    description = item.description.create(description_params)
+    respond_with item, description
+  end
+
+  def destroy
+    item = Item.find(params[:item_id])
+    item.description.destroy
+  end
+
+  private
+  def description_params
+    params.require(:description).permit(:body, :size, :color)
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,19 @@
 class ItemsController < ApplicationController
+
+  def index
+    respond_with Item.all
+  end
+
+  def create
+    respond_with Post.create(post_params)
+  end
+
+  def show
+    respond_with Post.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:post).permit(:name)
+  end
 end
