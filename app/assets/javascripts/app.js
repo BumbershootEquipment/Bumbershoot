424e0aa36774ef84a35a92e1888ea82fbc68aae7
CarsonBills <CarsonMBills@Gmail.com>
2015-08-05 15:15:38 -0400

    item pages viewable

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,7 +9,13 @@ function($stateProvider, $urlRouterProvider) {
 			url: "/home",
 			templateUrl: "/home.html",
 			controller: "MainCtrl"
+		})
+		.state("items", {
+			url: "/items/{id}",
+			templateUrl: "/items.html",
+			controller: "ItemsCtrl"
 		});
+
 	$urlRouterProvider.otherwise("home");
 }])
 
@@ -17,16 +23,37 @@ function($stateProvider, $urlRouterProvider) {
 "$scope",
 "items",
 function($scope, items){
+	$scope.addItem = function(){
+		if(!$scope.name || $scope.name === ""){return;}
+		$scope.items.push({
+			name: $scope.name,
+			category: "Misc.",
+			image: "Example Image",
+			details: {
+				description: "example description",
+				color: "Example Colors"
+			}
+		});
+		$scope.name = "";
+	}
 	$scope.items = items.items;
+}])
+
+.controller("ItemsCtrl", [
+"$scope",
+"$stateParams",
+"items",
+function($scope, $stateParams, items){
+	$scope.item = items.items[$stateParams.id];
 }])
 
 .factory("items", [function(){
 	var o = {
 		items: [
-			{name: "tent", category: "sleep", image: "Picture of a tent"},
-			{name: "stove", category: "cook", image: "Picture of a stove"},
-			{name: "lantern", category: "light", image: "Picture of a lantern"},
-			{name: "fishing pole", category: "recreation", image: "Picture of a fishing pole"}
+			{name: "tent", category: "sleep", image: "Picture of a tent", details: {color: "blue", size: "big"} },
+			{name: "stove", category: "cook", image: "Picture of a stove", details: {color: "blue", size: "big"} },
+			{name: "lantern", category: "light", image: "Picture of a lantern", details: {color: "blue", size: "big"} },
+			{name: "fishing pole", category: "recreation", image: "Picture of a fishing pole", details: {color: "blue", size: "big"} }
 		]};
 	return o;
 }])
