675b75859b4db2fb77681142fb34033adf8d7bb8
CarsonBills <CarsonMBills@Gmail.com>
2015-08-05 15:52:16 -0400

    add detail for product

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -45,15 +45,24 @@ function($scope, items){
 "items",
 function($scope, $stateParams, items){
 	$scope.item = items.items[$stateParams.id];
+	$scope.addDetail = function(){
+		if($scope.description === ""){return;}
+		$scope.item.details = {
+			description: $scope.description,
+			size: $scope.size,
+			color: $scope.color
+		};
+		$scope.description = "";
+	}
 }])
 
 .factory("items", [function(){
 	var o = {
 		items: [
-			{name: "tent", category: "sleep", image: "Picture of a tent", details: {color: "blue", size: "big"} },
-			{name: "stove", category: "cook", image: "Picture of a stove", details: {color: "blue", size: "big"} },
-			{name: "lantern", category: "light", image: "Picture of a lantern", details: {color: "blue", size: "big"} },
-			{name: "fishing pole", category: "recreation", image: "Picture of a fishing pole", details: {color: "blue", size: "big"} }
+			{name: "tent", category: "sleep", image: "Picture of a tent", details: {color: "blue", size: "big", description: "example desc."} },
+			{name: "stove", category: "cook", image: "Picture of a stove", details: {color: "blue", size: "big", description: "example desc."} },
+			{name: "lantern", category: "light", image: "Picture of a lantern", details: {color: "blue", size: "big", description: "example desc."} },
+			{name: "fishing pole", category: "recreation", image: "Picture of a fishing pole", details: {color: "blue", size: "big", description: "example desc."} }
 		]};
 	return o;
 }])
