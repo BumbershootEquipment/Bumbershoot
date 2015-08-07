424ea028fb5450ab8875be7e96df40dfdbca8df4
CarsonBills <CarsonMBills@Gmail.com>
2015-08-07 19:45:40 -0400

    Item has description as property

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805222122) do
+ActiveRecord::Schema.define(version: 20150807233636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150805222122) do
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
   add_index "items", ["category_id"], name: "index_items_on_category_id", using: :btree
