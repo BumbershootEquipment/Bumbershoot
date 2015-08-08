59e5215c716de2a88f5311a6615c99a4da37b90d
CarsonBills <CarsonMBills@Gmail.com>
2015-08-08 04:21:29 -0400

    user creation and login/logout:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  after_filter :set_csrf_cookie_for_ng
+
   respond_to :json
+
+  def set_csrf_cookie_for_ng
+    cookies["XSRF-TOKEN"] = form_authenticity_token if protect_against_forgery?
+  end
 
   def angular
     render "layouts/application"
+  end
+
+  protected
+  def verified_request?
+    super || form_authenticity_token == request.headers["X-XSRF-TOKEN"]
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 end
