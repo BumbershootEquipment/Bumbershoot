07099f36bc77d7703631ee83a101500b61416079
CarsonBills <CarsonMBills@Gmail.com>
2015-08-08 11:03:53 -0400

    moves paperclip model requirements to correct model, item... oops.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,12 @@ class Item < ActiveRecord::Base
   belongs_to :category
   has_one :description
 
+  has_attached_file :image, :styles => {
+    :thumb => "100x100>",
+    :large => "500x500>"
+  }
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   def as_json(options = {})
     super(options.merge(include: :description))
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_attached_file :image, :styles => {
-    :thumb => "100x100>",
-    :large => "500x500>"
-  }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
