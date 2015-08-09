d5da673151865fd70745a6682e7eb677f417c7b9
CarsonBills <CarsonMBills@Gmail.com>
2015-08-09 15:12:39 -0400

    image work around

--- app/assets/javascripts/categories/categoriesCtrl.js
+++ app/assets/javascripts/categories/categoriesCtrl.js
@@ -9,7 +9,7 @@ function($scope, Upload, categories, category){
 	$scope.addItem = function(){
 		if(!$scope.name || $scope.name === ""){return;}
 		categories.addItem(category.id, {
-			name: $scope.name,
+			name: $scope.name
 		}).success(function(item){
 			$scope.category.items.push(item);
 		});
