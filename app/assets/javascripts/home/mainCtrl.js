5146cf07997f11b63584a67b9dfb67f4bdf1f23b
CarsonBills <CarsonMBills@Gmail.com>
2015-08-11 21:48:18 -0400

    ladning image set

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -1,34 +1,13 @@
 angular.module("bumbershoot")
 .controller("MainCtrl", [
 "$scope",
-"categories",
-function($scope, categories){
-	$scope.addCategory = function(){
-		if(!$scope.name || $scope.name === ""){return;}
-		categories.create({
-			name: $scope.name
-		});
-		$scope.name = "";
-	}
-	$scope.categories = categories.categories
+function($scope){
 }])
 
 
 
 
 
-
-
-	// $scope.category = categories[$stateParams.id];
-	// $scope.addItem = function(){
-	// 	if(!$scope.item_name || $scope.item_name === ""){return;}
-	// 	items.create(
-	// 		{name: $scope.item_name,
-	// 		category: $scope.category}
-	// 	);
-	// 	$scope.item_name = "";
-	// }
-	// $scope.items = items.items;
 
 
 
