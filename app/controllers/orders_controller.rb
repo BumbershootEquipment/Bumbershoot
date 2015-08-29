9fda69e8a3b1b3c6eafc21b0544793de3ee06659
CarsonBills <CarsonMBills@Gmail.com>
2015-08-29 19:56:40 -0400

    orders belong to items not users

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,13 @@ class OrdersController < ApplicationController
   before_filter :authenticate_user!, only: [:create]
 
   def create
+    item = Item.find(params[:item_id])
 
+  end
+
+  private
+
+  def order_params
+    params.require(:order)
   end
 end
