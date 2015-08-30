655ba381f834c09719a06728044490ab58fe4f1c
CarsonBills <CarsonMBills@Gmail.com>
2015-08-30 10:41:43 -0400

    makes index of orders available

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,14 @@
 class OrdersController < ApplicationController
   before_filter :authenticate_user!, only: [:create]
 
+  def index
+    respond_with Order.all
+  end
+
   def create
     item = Item.find(params[:item_id])
-
+    order = item.orders.create(order_params)
+    respond_with order
   end
 
   private
