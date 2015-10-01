956f9135926062e6c776fadc3a181c8c2249cfd6
CarsonBills <CarsonMBills@Gmail.com>
2015-10-01 13:59:58 -0400

    email methods added to UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,19 @@
 class UserMailer < ActionMailer::Base
-  default from: "bumbershootequip@example.com"
+  default from: "bumbershootequip@gmail.com"
 
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: "Welcome to Bumbershoot")
+  end
+
+  def community_order(user, owner)
+    @user = user
+    @owner = owner
+    mail(to: @user.email, subject: "Community Equipment Order")
+  end
+
+  def corporate_order(user)
+    @user = user
+    mail(to: @user.email, subject: "New order from Bumbershoot")
   end
 end
