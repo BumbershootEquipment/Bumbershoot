a6bcd586abc0c470835240f8f6d679799c6df5f9
CarsonBills <CarsonMBills@Gmail.com>
2015-10-12 11:47:36 -0400

    seed file works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email: "BumbershootEquip@Gmail.com", password: ENV['dmin_password'], username: "Admin")
+User.create(email: "BumbershootEquip@Gmail.com", password: ENV['admin_password'], username: "Admin")
 
 categories = Category.create([{name: "Sleep"}, {name: "Cook"}, {name: "Recreation"}, {name: "Miscellaneous"}])
 
@@ -21,27 +21,27 @@ descriptions = [
   {size: "4 Person",
   color: "Grey",
   body: "The multi-diameter pole configuration enables the walls to stay steep and aggressively arch toward the top to produce a large livable space.",
-  }
+  },
   {size: "2 Person",
   color: "Grey",
   body: "This 2-person tent is so light, you'll almost forget it's in your pack. With ample vestibule space, compact folding poles and high sidewall protection, it's got everything you need for 3 seasons.",
-  }
+  },
   {size: "60 Liter Capacity",
   color: "Blue",
   body: "Hit the wilderness with the light, simple and value-conscious Osprey Volt 60 pack. Enjoy its comfortable fit with all the essential features, but without extraneous bells and whistles.",
-  }
+  },
   {size: "8.5 x 6.5 x 2 inches",
   color: "Yellow",
   body: "Pared down to a weight-saving bare minimum, this Adventure Medical Ultralight / Watertight .7 first-aid kit contains the most essential supplies.",
-  }
+  },
   {size: "27 x 17 inches",
   color: "Blue",
   body: "Take it camping, fishing or to the kid's game—the lightweight GCI Quik-E-Seat chair offers comfort and easy portability.",
-  }
+  },
   {size: "12.3 x 2.25 inches",
   color: "Black",
   body: "A third generation of the legendary Maglite, this water-resistant LED flashlight casts a powerful beam with 3 D-cell batteries. It offers a range of modes and spot-to-flood to light up your world.",
-  }
+  },
   {size: "27.5 inches",
   color: "Black & White",
   body: "Just a standard soccer ball, nothing to see here.",
@@ -50,62 +50,62 @@ descriptions = [
 
 
 descriptions.each do |description|
-  Drescription.create(size: description[0], color: description[1], body: description[2])
+  Description.create(size: description[0], color: description[1], body: description[2])
 end
 
 
 items = [
   {name: "Coleman Stove",
   category: 2,
-  paypalID: ,
+  paypalID: "",
   price: 10,
   description: Description.find(1)
   },
   {name: "Therm-a-rest Sleeping Pad",
   category: 1,
-  paypalID: ,
+  paypalID: "",
   price: 2,
   description: Description.find(2)
   },
   {name: "Kelty - 4 Person Tent",
   category: 1,
-  paypalID: ,
+  paypalID: "",
   price: 35,
   description: Description.find(3)
   },
   {name: "Kelty Lightweight - 2 Person Tent",
   category: 1,
-  paypalID: ,
+  paypalID: "",
   price: 15,
   description: Description.find(4)
   },
   {name: "Osprey Backpack",
   category: 1,
-  paypalID: ,
+  paypalID: "",
   price: 10,
   description: Description.find(5)
   },
   {name: "First Aid Kit",
   category: 4,
-  paypalID: ,
+  paypalID: "",
   price: 5,
   description: Description.find(6)
   },
   {name: "CGI Folding Chair",
   category: 4,
-  paypalID: ,
+  paypalID: "",
   price: 5,
   description: Description.find(7)
   },
   {name: "Maglite LED Flashlight",
   category: 4,
-  paypalID: ,
+  paypalID: "",
   price: 5,
   description: Description.find(8)
   },
   {name: "Soccer Ball",
   category: 3,
-  paypalID: ,
+  paypalID: "",
   price: 5,
   description: Description.find(9)
   },
